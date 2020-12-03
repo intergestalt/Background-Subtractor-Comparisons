a5305d65b8bc2e5e1610f7dfcba5f7737cda39dc
Holger H <holgheiss@gmail.com>
2020-12-03 21:50:53 +0100

    option to load external .mov

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
+MOV=${1:-./demo.mov}
+
+echo $MOV
+
 xhost + "$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')"
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 docker build --tag bgsub .
-docker run -e DISPLAY="$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')":0 --rm bgsub
+docker run -v $1:/app/demo.mov -e DISPLAY="$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')":0 --rm bgsub
